fe18a14e8fccc2ecb161309ed2007c2a346b424f
Rob Verbeek <r0932067@student.thomasmore.be>
2023-12-14 15:46:15 +0100

    fixing routine?

--- Case_Study/Models/Routine.cs
+++ Case_Study/Models/Routine.cs
@@ -4,7 +4,7 @@ namespace Case_Study.Models
 {
     public class Routine
     {
-        public SystemProperties Sys {  get; set; }
+        public SystemProperties? Sys {  get; set; }
         public required string RoutineName { get; set; }
 
         public List<Exercise>? Exercises { get; set; }
